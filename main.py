7c7626e71df8889255b00508ad9f1fc99bf07ca9
Stas <s_anufriev@list.ru>
2023-05-26 21:00:39 +0700

    проверка на запрещенные символы, ошибки выводим в телегу, называем скачанный именем и текущей датой создания во избежания конфликта имен, удаление mp4 после конвертации в mp3, рефакторинг

--- main.py
+++ main.py
@@ -1,107 +1,146 @@
+import datetime
 import logging
+import os
+import re
+import subprocess
+import threading
 
 import telebot
 from PyQt5.QtWidgets import QApplication, QMainWindow, QProgressBar, QLabel, QLineEdit, QPushButton, QFileDialog, QMessageBox
 from yt_dlp import YoutubeDL
-import subprocess
-import threading
-import re
+
+
+
+
+def clean_filename(text: str) -> str:
+    # запрещенные символы
+    forbidden = '[/\\\:\*\?"<>\|]'
+    # заменяем запрещенные символы на _
+    clean_text = re.sub(forbidden, '-', text)
+    return clean_text
+
 
 def output_t(token, chat_id, msg):
     bot = telebot.TeleBot(token)
+    if msg is not None:
+        bot.send_message(chat_id=chat_id, text=msg)
+    else:
+        bot.send_message(chat_id=chat_id, text="running")
+    bot.stop_polling()
 
-    bot.send_message(chat_id=chat_id, text="running")
-    bot.send_message(chat_id=chat_id, text=msg)
-
-    bot.stop_bot()
 
 class MainWindow(QMainWindow):
-   def __init__(self):
-       super().__init__()
+    def __init__(self):
+        super().__init__()
 
-       self.setWindowTitle("Скачивание видео c YouTube")
-       self.resize(350, 200)
-       self.label = QLabel(self)
-       self.label.setText("Вставить ссылку на видео")
-       self.label.move(20, 30)
+        self.setWindowTitle("Скачивание видео c YouTube")
+        self.resize(350, 200)
 
-       self.line_edit = QLineEdit(self)
-       self.line_edit.move(20, 60)
-       self.line_edit.resize(280, 30)
+        self.label = QLabel(self)
+        self.label.setText("Вставить ссылку на видео")
+        self.label.move(20, 30)
 
-       self.folder_button = QPushButton(self)
-       self.folder_button.setText("Куда скачать")
-       self.folder_button.move(20, 100)
-       self.folder_button.clicked.connect(self.choose_folder)
+        self.line_edit = QLineEdit(self)
+        self.line_edit.move(20, 60)
+        self.line_edit.resize(280, 30)
 
-       self.folder_label = QLabel(self)
-       self.folder_label.move(120, 104)
+        self.folder_button = QPushButton(self)
+        self.folder_button.setText("Куда скачать")
+        self.folder_button.move(20, 100)
+        self.folder_button.clicked.connect(self.choose_folder)
 
-       self.download_button = QPushButton(self)
-       self.download_button.setText("Скачать")
-       self.download_button.move(20, 140)
-       self.download_button.clicked.connect(self.download_video)
+        self.folder_label = QLabel(self)
+        self.folder_label.move(120, 104)
+        self.folder_label.setFixedWidth(240)
+        # self.folder_label.setReadOnly(True)
 
-       self.progressbar = QProgressBar(self)
-       self.progressbar.setGeometry(20, 170, 280, 20)
+        self.download_button = QPushButton(self)
+        self.download_button.setText("Скачать")
+        self.download_button.move(20, 140)
+        self.download_button.clicked.connect(self.download_video)
 
-   def download_and_convert(self, URL, path, progressbar):
-       options = {
-           'format': 'bestaudio[ext=mp3]/best',
-           'extractaudio': True,
-           'outtmpl': f'{path}/%(title)s.%(ext)s',
-           'noplaylist': True,
-           'progress_hooks': [lambda d: progressbar.setValue(int(d['downloaded_bytes'] * 100 / d['total_bytes']))]
-       }
-       logger = logging.getLogger(__name__)
-       try:
-           progressbar.setFormat("Загрузка видео")
-           with YoutubeDL(options) as ydl:
-               ydl.download([URL])
-           progressbar.setFormat("Видео загружено")
-           video_title = ydl.extract_info(URL, download=False).get('title', None)
-           input_video = f"{path}/{video_title}.mp4"
-           output_audio = f"{path}/{video_title}.mp3"
-           progressbar.setFormat("Конвертируем в аудио файл")
-           subprocess.call(
-               ["ffmpeg", "-i", input_video, "-vn", "-ar", "44100", "-ac", "2", "-b:a", "192k", output_audio])
-           progressbar.setFormat("Успешно завершено")
-       except Exception as e:
-           logger.exception("Ошибка: %s", str(e))
+        self.progressbar = QProgressBar(self)
+        self.progressbar.setGeometry(20, 170, 280, 20)
 
-   def choose_folder(self):
-       folder_path = QFileDialog.getExistingDirectory(self, "Выбрать папку")
-       self.folder_label.setText(folder_path)
+    def download_and_convert(self, URL, path, progressbar):
+        os.system('cls' if os.name == 'nt' else 'clear')
+        date = datetime.datetime.now().strftime('%Y-%m-%d_%H-%M-%S')
+        try:
+            options = {
+                'format': 'bestaudio[ext=mp3]/best',
+                'extractaudio': True,
+                'outtmpl': f"{path}/%(title)s_{date}.%(ext)s",
+                'noplaylist': True,
+                'progress_hooks': [lambda d: progressbar.setValue(int(d['downloaded_bytes'] * 100 / d['total_bytes']))]
+            }
 
-   def showq(self):
-       reply = QMessageBox.warning(self, "ntcnfsfjk",
-                                    "sekjfen",
-                                    QMessageBox.Ok)
+            logger = logging.getLogger(__name__)
 
-   def download_video(self):
-       if not self.line_edit.text():
-           reply = QMessageBox.warning(self, "Пустая ссылка",
-                                        "укажите правильную ссылку на видео YouTube",
-                                        QMessageBox.Ok)
+            progressbar.setFormat("Загрузка видео")
 
-       elif not self.folder_label.text():
-           reply = QMessageBox.warning(self, "Пустой путь", "нажмите на кнопку <Выбрать папку> и выберете место куда скачать аудио",
-                                        QMessageBox.Ok)
+            with YoutubeDL(options) as ydl:
+                ydl.download([URL])
 
-       # Проверка правильности ссылки с помощью регулярного выражения
-       url_pattern = r"^(https?\:\/\/)?(www\.youtube\.com|youtu\.?be)\/.+$"
-       if not re.match(url_pattern, self.line_edit.text()):
-           reply = QMessageBox.warning(self, "Некорректная ссылка",
-                                       "Укажите правильную ссылку на видео YouTube",
-                                        QMessageBox.Ok)
+            progressbar.setFormat("Видео загружено")
 
-       else:
-           progress_thread = threading.Thread(target=self.download_and_convert, args=(self.line_edit.text(), self.folder_label.text(), self.progressbar))
-           progress_thread.start()
+            video_title = ydl.extract_info(URL, download=False).get('title', None)
+            input_video = f"{path}/{clean_filename(video_title)}_{date}.mp4"
+            output_audio = f"{path}/{clean_filename(video_title)}.mp3"
+
+            progressbar.setFormat("Конвертирование в аудио файл")
+
+            if os.path.isfile(output_audio):
+                os.remove(output_audio)
+
+            subprocess.call(["ffmpeg", "-i", input_video, "-vn", "-ar", "44100", "-ac", "2", "-b:a", "192k", output_audio])
+
+            os.remove(input_video)
+
+            progressbar.setFormat("Успешно завершено")
+
+        except KeyError as k:
+            output_t(token, id, "(ОШИБКА) \nв директорию качается тот же файл\n" + str(k))
+            logger.exception("Ошибка: %s", str(k))
+
+        except Exception as e:
+            output_t(token, id, str(e))
+            logger.exception("Ошибка: %s", str(e))
+
+
+
+    def choose_folder(self):
+        folder_path = QFileDialog.getExistingDirectory(self, "Выбрать папку")
+        self.folder_label.setText(folder_path)
+
+    def download_video(self):
+        if not self.line_edit.text():
+            reply = QMessageBox.warning(self, "Пустая ссылка",
+                                         "Укажите правильную ссылку на видео YouTube",
+                                         QMessageBox.Ok)
+
+        elif not self.folder_label.text():
+            reply = QMessageBox.warning(self, "Пустой путь",
+                                         "Нажмите на кнопку <Куда скачать> и выберете место куда скачать аудио",
+                                         QMessageBox.Ok)
+
+        # Проверка правильности ссылки с помощью регулярного выражения
+        url_pattern = r"^(https?\:\/\/)?(www\.youtube\.com|youtu\.?be)\/.+$"
+        if not re.match(url_pattern, self.line_edit.text()):
+            reply = QMessageBox.warning(self, "Некорректная ссылка",
+                                         "Укажите правильную ссылку на видео YouTube",
+                                         QMessageBox.Ok)
+
+        else:
+            progress_thread = threading.Thread(target=self.download_and_convert, args=(self.line_edit.text(), self.folder_label.text(), self.progressbar))
+            progress_thread.start()
 
 
 if __name__ == "__main__":
-   app = QApplication([])
-   window = MainWindow()
-   window.show()
-   app.exec_()
+    try:
+        output_t(token, id, msg=None)
+        app = QApplication([])
+        window = MainWindow()
+        window.show()
+        app.exec_()
+    except Exception as e:
+        output_t(token, id, str(e))
