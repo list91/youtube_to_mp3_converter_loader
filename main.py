7ae0022002c9cd3ecf7639e8629e88f5e4084828
Stas <s_anufriev@list.ru>
2023-05-26 10:50:37 +0700

    startCommit

---
+++ main.py
@@ -0,0 +1,72 @@
+from PyQt5.QtWidgets import QApplication, QMainWindow, QProgressBar, QLabel, QLineEdit, QPushButton, QFileDialog, QMessageBox
+from yt_dlp import YoutubeDL
+import subprocess
+import threading
+
+def download_and_convert(URL, path, progressbar):
+    options = {
+        'format': 'bestaudio[ext=mp3]/best',
+        'extractaudio': True,
+        'outtmpl': f'{path}/%(title)s.%(ext)s',
+        'noplaylist': True,
+        'progress_hooks': [lambda d: progressbar.setValue(int(d['downloaded_bytes']*100/d['total_bytes']))]
+    }
+    with YoutubeDL(options) as ydl:
+        ydl.download([URL])
+    video_title = ydl.extract_info(URL, download=False).get('title', None)
+    input_video = f"{path}/{video_title}.mp4"
+    output_audio = f"{path}/{video_title}.mp3"
+    subprocess.call(["ffmpeg", "-i", input_video, "-vn", "-ar", "44100", "-ac", "2", "-b:a", "192k", output_audio])
+
+class MainWindow(QMainWindow):
+    def __init__(self):
+        super().__init__()
+
+        self.setWindowTitle("Скачивание видео c YouTube")
+        self.resize(350, 200)
+        self.label = QLabel(self)
+        self.label.setText("Вставить ссылку на видео")
+        self.label.move(20, 30)
+
+        self.line_edit = QLineEdit(self)
+        self.line_edit.move(20, 60)
+        self.line_edit.resize(280, 30)
+
+        self.folder_button = QPushButton(self)
+        self.folder_button.setText("Куда скачать")
+        self.folder_button.move(20, 100)
+        self.folder_button.clicked.connect(self.choose_folder)
+
+        self.folder_label = QLabel(self)
+        self.folder_label.move(120, 104)
+
+        self.download_button = QPushButton(self)
+        self.download_button.setText("Скачать")
+        self.download_button.move(20, 140)
+        self.download_button.clicked.connect(self.download_video)
+
+        self.progressbar = QProgressBar(self)
+        self.progressbar.setGeometry(20, 170, 280, 20)
+
+    def choose_folder(self):
+        folder_path = QFileDialog.getExistingDirectory(self, "Выбрать папку")
+        self.folder_label.setText(folder_path)
+
+    def download_video(self):
+        if not self.line_edit.text():
+            reply = QMessageBox.warning(self, "Пустая ссылка",
+                                        "укажите правильную ссылку на видео YouTube",
+                                        QMessageBox.Ok)
+
+        if not self.folder_label.text():
+            reply = QMessageBox.warning(self, "Пустой путь", "нажмите на кнопку <Выбрать папку> и выберете место куда скачать аудио",
+                                        QMessageBox.Ok)
+        else:
+            progress_thread = threading.Thread(target=download_and_convert, args=(self.line_edit.text(), self.folder_label.text(), self.progressbar))
+            progress_thread.start()
+
+if __name__ == "__main__":
+    app = QApplication([])
+    window = MainWindow()
+    window.show()
+    app.exec_()
